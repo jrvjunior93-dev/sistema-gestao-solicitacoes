c53204a2b40bb7e07780724bf5e6f52a664458f8
jose-ricardo <jrvjunior93@gmail.com>
2026-02-04 16:45:33 -0300

    Correção de erro no arquivo s2.js no regex

--- backend/src/services/s3.js
+++ backend/src/services/s3.js
@@ -27,7 +27,7 @@ function getKeyFromUrl(url) {
     const bucket = process.env.AWS_S3_BUCKET;
     const parsed = new URL(url);
     if (!parsed.hostname.startsWith(`${bucket}.s3`)) return null;
-    return parsed.pathname.replace(/^\\//, '');
+    return parsed.pathname.replace(/^\//, '');
   } catch (error) {
     return null;
   }
